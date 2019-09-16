a5888c7565be77aa0cf2851f059ef3fe0575278e
issuser <issuser@ISS620002000237>
2019-09-16 16:08:43 +0800

    更新test1

--- src/org/test/demo/Test1.java
+++ src/org/test/demo/Test1.java
@@ -1,7 +1,7 @@
 package org.test.demo;
 
-public class Main {
+public class Test1 {
     public static void main(String[] args) {
-        System.out.println("test1");
+        System.out.println("");
     }
 }
